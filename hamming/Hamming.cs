2afa8d2757c3a90d0c25c3664ae4a15ba7733a60
Nathan <nathanjwtx@nathanjwtx.io>
2018-12-06 20:05:06 -0600

    Completed Hamming

--- hamming/Hamming.cs
+++ hamming/Hamming.cs
@@ -4,6 +4,20 @@ public static class Hamming
 {
     public static int Distance(string firstStrand, string secondStrand)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var result = 0;
+        if (firstStrand.Length != secondStrand.Length)
+        {
+            throw new ArgumentException();
+        }
+
+        for (int i = 0; i < firstStrand.Length; i++)
+        {
+            if (firstStrand[i] != secondStrand[i])
+            {
+                result++;
+            }
+        }
+
+        return result;
     }
 }
