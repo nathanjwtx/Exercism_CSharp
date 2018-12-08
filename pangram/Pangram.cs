f719a3ac839082df4180cdb552943f8804ff4dc0
Nathan <nathanjwtx@nathanjwtx.io>
2018-12-08 07:37:02 -0600

    Completed Pangram

---
+++ pangram/Pangram.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        var alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        foreach (var x in alphabet)
+        {
+            if (!input.ToLower().Contains(x))
+            {
+                return false;
+            }
+        }
+
+        return true;
+
+        //online solution
+        //return alphabet.All(input.ToLower().Contains);
+    }
+}
