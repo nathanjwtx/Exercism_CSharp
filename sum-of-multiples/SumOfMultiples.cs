d1cb8cb54aedd2372b9119f6140c8d3c39f9262f
Nathan Waterman <nathanjwtx@nathanjwtx.io>
2018-12-10 15:25:46 -0600

    Completed Sum Of Multiples

--- sum-of-multiples/SumOfMultiples.cs
+++ sum-of-multiples/SumOfMultiples.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
 
 public static class SumOfMultiples
 {
     public static int Sum(IEnumerable<int> multiples, int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        List<int> numbers = new List<int>();
+        for (int i = 1; i < max; i++)
+        {
+            numbers.Add(i);
+        }
+
+        var interimResult = (from x in numbers
+            from y in multiples
+            where y != 0 // prevents an error if one of the values in multiples is 0
+            where x % y == 0
+            select x).ToList();
+
+        var result = interimResult.Distinct().ToList();
+
+        Console.WriteLine(string.Join(", ", interimResult));
+        Console.WriteLine(result.Sum());
+
+        return result.Sum();
     }
 }
---
+++ sum-of-multiples/Testing/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var multiples = new[] {3, 0};
+            SumOfMultiples.Sum(multiples, 4);
+        }
+    }
+}
