26196b5dd3897eda68091cd96c8ad9ba741e7adf
Nathan Waterman <nathanjwtx@nathanjwtx.io>
2018-12-11 13:43:12 -0600

    Completed Darts

--- darts_WIP/Darts.cs
+++ darts_WIP/Darts.cs
@@ -4,6 +4,18 @@ public static class Darts
 {
     public static int Score(double x, double y)
     {
-       throw new NotImplementedException("You need to implement this function");
+        if (x >= 0 && x <= 1 && y >= 0 && y <= 1)
+        {
+            return 10;
+        }
+        if (x >= 0 && x <= 5 && y >=0 && y <= 5)
+        {
+            return 5;
+        }
+        if (x >= 0 && x <= 10 && y >= 0 && y <= 10)
+        {
+            return 1;
+        }
+        return 0;
     }
 }
