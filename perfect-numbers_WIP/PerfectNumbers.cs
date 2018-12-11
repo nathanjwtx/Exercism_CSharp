9bf6765f403f656cc88ba9782275fe0da71baf56
Nathan Waterman <nathanjwtx@nathanjwtx.io>
2018-12-11 13:17:24 -0600

    Completed PerfectNumbers

--- perfect-numbers_WIP/PerfectNumbers.cs
+++ perfect-numbers_WIP/PerfectNumbers.cs
@@ -11,6 +11,25 @@ public static class PerfectNumbers
 {
     public static Classification Classify(int number)
     {
-        throw new NotImplementedException();
+        if (number <= 0)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+        var sum = 0;
+        for (int i = 1; i < number; i++)
+        {
+            if (number % i == 0)
+            {
+                sum += i;
+            }
+        }
+        if (sum == number)
+        {
+            return Classification.Perfect;
+        } else if (sum > number)
+        {
+            return Classification.Abundant;
+        }
+        return Classification.Deficient;
     }
 }
---
+++ perfect-numbers_WIP/Testing/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = PerfectNumbers.Classify(8);
+            Console.WriteLine(result);
+        }
+    }
+}
