c3b0925a95ef488cfc674fac6b1f7df843f360af
Nathan Waterman <nathanjwtx@nathanjwtx.io>
2018-12-11 15:23:05 -0600

    Started on Largest Series Product

--- largest-series-product_WIP/LargestSeriesProduct.cs
+++ largest-series-product_WIP/LargestSeriesProduct.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Linq;
+using Xunit.Sdk;
 
 public static class LargestSeriesProduct
 {
-    public static long GetLargestProduct(string digits, int span) 
+    public static long GetLargestProduct(string digits, int span)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var result = 0;
+        var temp = 0;
+        for (int i = 0; i < digits.Length - span; i++)
+        {
+            var sub = digits.Substring(i, span);
+            temp = digits.Substring(i, span).Aggregate(1, (acc, val) => acc * val);
+            Console.WriteLine(sub);
+//            temp = 0;
+//            for (int j = 0; j < span; j++)
+//            {
+//                temp += digits[i + j];
+//            }
+            temp = 0;
+        }
+
+        
+
+        return temp;
     }
 }
---
+++ largest-series-product_WIP/Testing/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = LargestSeriesProduct.GetLargestProduct("123456", 2);
+
+            Console.WriteLine(result);
+        }
+    }
+}
