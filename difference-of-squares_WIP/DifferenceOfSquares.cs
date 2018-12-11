31046e61fedcd75b7c7e3c8fc7f410fef3d87ef2
Nathan Waterman <nathanjwtx@nathanjwtx.io>
2018-12-11 14:44:24 -0600

    Completed Difference Of Squares

--- difference-of-squares_WIP/DifferenceOfSquares.cs
+++ difference-of-squares_WIP/DifferenceOfSquares.cs
@@ -1,19 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class DifferenceOfSquares
 {
     public static int CalculateSquareOfSum(int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var nums = MakeNumbersList(max);
+
+        return Convert.ToInt32(Math.Pow(nums.Sum(), 2));
     }
 
     public static int CalculateSumOfSquares(int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var nums = MakeNumbersList(max);
+
+        return Convert.ToInt32(nums.Sum(n => n * n));
     }
 
     public static int CalculateDifferenceOfSquares(int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return Convert.ToInt32(Math.Abs(CalculateSumOfSquares(max) - CalculateSquareOfSum(max)));
+    }
+
+    private static List<int> MakeNumbersList(int max)
+    {
+        return Enumerable.Range(1, max).ToList();
     }
 }
---
+++ difference-of-squares_WIP/Testing/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 10;
+
+            var nums = Enumerable.Range(1, 10).ToList();
+            // nums.ForEach(n => Console.WriteLine(n)); can be replaced by below using Method Group
+            nums.ForEach(Console.WriteLine);
+
+            var sqSum = Math.Pow(nums.Sum(), 2);
+            var sumSq = nums.Sum(n => n * n);
+
+            Console.WriteLine(sqSum);
+            Console.WriteLine(sumSq);
+        }
+    }
+}
