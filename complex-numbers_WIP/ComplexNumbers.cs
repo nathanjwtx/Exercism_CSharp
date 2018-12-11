12d57116d009a87b7328f8c6f46400b1ea5d9a95
Nathan Waterman <nathanjwtx@nathanjwtx.io>
2018-12-11 08:51:42 -0600

    Complex Numbers in process

--- complex-numbers_WIP/ComplexNumbers.cs
+++ complex-numbers_WIP/ComplexNumbers.cs
@@ -2,33 +2,43 @@ using System;
 
 public struct ComplexNumber
 {
+    private double _real;
+    private double _imaginary;
+    private double _i;
     public ComplexNumber(double real, double imaginary)
     {
+        _real = real;
+        _imaginary = imaginary;
+        _i = -1;
     }
 
     public double Real()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return _real;
     }
 
     public double Imaginary()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return _imaginary;
     }
 
     public ComplexNumber Mul(ComplexNumber other)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var firsts = _real * other._real;
+        var outers = _real * other._imaginary;
+        var inners = _imaginary * other._real;
+        var lasts = _imaginary * other._imaginary * -1;
+        return new ComplexNumber(firsts + lasts, outers + inners);
     }
 
     public ComplexNumber Add(ComplexNumber other)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return new ComplexNumber(_real + other._real, _imaginary + other._imaginary);
     }
 
     public ComplexNumber Sub(ComplexNumber other)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return new ComplexNumber(_real - other._real, _imaginary - other._imaginary);
     }
 
     public ComplexNumber Div(ComplexNumber other)
---
+++ complex-numbers_WIP/Testing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = new ComplexNumber(3, 2);
+            var num1 = new ComplexNumber(1, 7);
+            var result = num.Mul(num1);
+            Console.WriteLine(result.Real());
+            Console.WriteLine(result.Imaginary());
+
+        }
+    }
+}
