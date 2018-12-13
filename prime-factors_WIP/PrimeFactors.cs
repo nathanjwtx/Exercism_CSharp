b2e0c46b672232ee5e171df9cd7d4ce510301d4f
Nathan Waterman <nathanjwtx@nathanjwtx.io>
2018-12-13 12:00:06 -0600

    Working on Prime Factors

--- prime-factors_WIP/PrimeFactors.cs
+++ prime-factors_WIP/PrimeFactors.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
 public static class PrimeFactors
 {
+    public static List<int> Primes = new List<int>();
+    private static long PreviousNumber = 0;
+    
     public static int[] Factors(long number)
     {
-        throw new NotImplementedException();
+        Console.WriteLine(number);
+        if (number == 1)
+        {
+            return Primes.ToArray();
+        }
+        for (var i = 2; i <= number; i++)
+        {
+            if (number > PreviousNumber && PreviousNumber != 0)
+            {
+//                Console.WriteLine("End");
+                break;
+            }
+            if (number % i == 0)
+            {
+//                Console.WriteLine(i);
+                Primes.Add(i);
+                PreviousNumber = number;
+                Factors(number / i);
+            }
+        }
+
+        return Primes.ToArray();
     }
 }
---
+++ prime-factors_WIP/Testing/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = PrimeFactors.Factors(93819012551);
+
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
