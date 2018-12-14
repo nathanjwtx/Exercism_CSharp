b8decfb24dad80528b818c936c172fc7ee2a1efa
Nathan Waterman <nathanjwtx@nathanjwtx.io>
2018-12-14 15:08:51 -0600

    Started on Rotational Cypher

--- rotational-cipher/RotationalCipher.cs
+++ rotational-cipher/RotationalCipher.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
 
 public static class RotationalCipher
 {
+    private const int UpperA = 65;
+    private const int UpperZ = 90;
+    private const int LowerA = 97;
+    private const int LowerZ = 122;
+
     public static string Rotate(string text, int shiftKey)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (shiftKey == 0 || shiftKey == 26)
+        {
+            return text;
+        }
+
+        var result = "";
+//        Console.WriteLine((int) 'Y');
+        Console.WriteLine((int) 'a');
+        foreach (char c in text)
+        {
+            // check for punctuation or space
+            if (char.IsPunctuation(c) || char.IsWhiteSpace(c))
+            {
+                result += c;
+            }
+            if ((int) c + shiftKey > 90)
+            {
+                
+                var x = (int) c + shiftKey - UpperZ;
+                result += Convert.ToChar(UpperA + x - 1);
+
+            }   
+        }
+
+        
+
+//        foreach (var i in interimResult)
+//        {
+//            result += (char) i;
+//        }
+        return result;
     }
 }
---
+++ rotational-cipher/Testing/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static RotationalCipher;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Rotate("Y", 2));
+        }
+    }
+}
