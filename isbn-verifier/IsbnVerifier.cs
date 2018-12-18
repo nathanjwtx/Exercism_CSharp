8190ae5a5220de99aae6481830022ca1063623e3
Nathan Waterman <nathanjwtx@nathanjwtx.io>
2018-12-18 12:13:04 -0600

    Completed ISBN Verifier

--- isbn-verifier/IsbnVerifier.cs
+++ isbn-verifier/IsbnVerifier.cs
@@ -1,9 +1,44 @@
 ﻿using System;
+using System.Linq;
 
 public static class IsbnVerifier
 {
     public static bool IsValid(string number)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var noDash = number.Replace("-", "");
+        Console.WriteLine(noDash);
+
+        if (noDash.Length != 10)
+        {
+            return false;
+        }
+
+        double result = 0;
+
+        for (int i = 10; i > 1; i--)
+        {
+            if (!char.IsNumber(noDash[10-i]))
+            {
+                return false;
+            }
+            result += char.GetNumericValue(noDash[10 - i]) * i;
+        }
+
+        if (noDash[9] == 'X')
+        {
+            result += 10;
+        }
+        else if (char.IsNumber(noDash[9]))
+        {
+            result += (char.GetNumericValue(noDash[9]));
+        }
+
+        Console.WriteLine(result % 11);
+
+        if (result % 11 == 0.0)
+        {
+            return true;
+        }
+        return false;
     }
 }
---
+++ isbn-verifier/Testing/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var isbn = "3-598-21508-8";
+
+            var number = IsbnVerifier.IsValid(isbn);
+            Console.WriteLine(number);
+        }
+    }
+}
