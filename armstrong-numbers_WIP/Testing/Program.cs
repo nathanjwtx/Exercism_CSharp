33d919ddfa50bae71da6f393c62f9bfcc9e22844
nathanjwtx <nathanjwtx@nathanjwtx.io>
2019-04-30 14:51:07 -0500

    Added Testing folder to Armstrong Numbers

---
+++ armstrong-numbers_WIP/Testing/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static ArmstrongNumbers.ArmstrongNumbers;
+
+namespace Testing
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine($"Is Armstrong Number: {IsArmstrongNumber(25)}");
+        }
+    }
+}
