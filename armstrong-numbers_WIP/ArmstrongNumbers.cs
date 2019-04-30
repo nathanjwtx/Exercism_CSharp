b4715575bb20715648e5d861abfbda3653f7ba0d
nathanjwtx <nathanjwtx@nathanjwtx.io>
2019-04-30 13:38:17 -0500

    Added Testing project to Armstrong Numbers

--- armstrong-numbers_WIP/ArmstrongNumbers.cs
+++ armstrong-numbers_WIP/ArmstrongNumbers.cs
@@ -1,9 +1,1 @@
-﻿using System;
-
-public static class ArmstrongNumbers
-{
-    public static bool IsArmstrongNumber(int number)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
-}
+﻿using System;public static class ArmstrongNumbers{    public static bool IsArmstrongNumber(int number)    {        throw new NotImplementedException("You need to implement this function.");    }}
